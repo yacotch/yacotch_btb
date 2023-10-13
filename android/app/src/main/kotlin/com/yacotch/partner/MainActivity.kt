f153f179ece359279c6e51a0a1b27cc4f6f16ef4
Esraa Abdrabo <91227030+esraaabdrabo@users.noreply.github.com>
2023-10-13 23:31:38 +0300

    3

---
+++ android/app/src/main/kotlin/com/yacotch/partner/MainActivity.kt
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+package com.yacotch.partner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
+=======
+package com.yacotch.partner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
+>>>>>>> 94fdc12b5108c23e5c2b11fad95c268d9cb37739
